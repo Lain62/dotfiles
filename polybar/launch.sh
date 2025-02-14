96d1bc36eabb250b00017793e0d3b9116d9486f3
lain62 <laimadelain@gmail.com>
2025-02-14 22:45:02 +0700

    removed monitor sh and used an i3 solution instead
    basically made it so that you run monitor sh commands when you restart
    i3 with super+shift+r

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,11 +4,20 @@
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# olybar bar1 2>&2 | tee -a /tmp/polybar2.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+# MONITOR=eDP polybar bar1 &
+# MONITOR=HMDI-A-0 polybar bar1 
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload bar1 &
+done
+
 
 echo "Bars launched..."
