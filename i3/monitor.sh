8c9b63db3f0cbdf9dd6f2f55b733d87ea2b00a2d
lain62 <laimadelain@gmail.com>
2025-02-14 23:01:30 +0700

    Used a new solution that actually works with i3
    Previous one didn't work as I hoped
    this time i used a script that will automatically detect how many
    monitors are set up and set it up accordingly

---
+++ i3/monitor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+MONITORCOUNT="$(xrandr --query | grep "\bconnected\b" | wc -l)"
+
+if [ $MONITORCOUNT == 2 ]; then
+	# Sets up my laptop monitor
+xrandr --output eDP --mode 1920x1080 --pos 0x0 --scale 1x1
+# Sets up xp-pen artist 12 (2nd gen) graphic tablet
+xrandr --output HDMI-A-0 --mode 1920x1080 --right-of eDP --scale 1x1
+else
+	xrandr --auto
+fi
+
